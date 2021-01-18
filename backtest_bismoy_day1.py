6c372b3fa4bee0b61263fc0258020a5f389299eb
Sonu <bismoy.sonu20@gmail.com>
2021-01-18 19:06:49 +0530

    W01D01 assign

---
+++ backtest_bismoy_day1.py
@@ -0,0 +1,40 @@
+print("Bismoy")
+##############
+print("Bismoy","Deep",sep=",")
+##############
+print(0)
+#############
+print(25.5)
+#############
+print(0,25.5,sep=",")
+#############
+print("Nifty's Close:{} and BankNifty's Close:{}".format(14434.4,32255.95))
+#############
+print("1st method:")
+
+for i in range(5):
+    print("$"*(i+1))
+#############
+print("2nd method:")
+
+for i in range(1,6):
+    for  j in range(1,i+1):
+        print("$",end="")
+    print(" ")
+############
+print(" ")
+print("Bismoy"+"27")
+#############
+print("Bismoy","27",sep="\n")
+
+print(" ")
+##############
+print("$"*16)
+##############
+
+#1.Invalid syntax(No parenthesis)
+#2.Invalid syntax(Shravan not recognized)
+#3.Invalid syntax as concat possible  only  in strings
+#4. Invalid syntax EOL error
+#5. Invalid syntax EOL error
+#6. Invalid syntax EOL error
