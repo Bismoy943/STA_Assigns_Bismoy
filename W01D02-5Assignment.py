c1f273d2edb7f258d7a067928c01cc2d2cfc99eb
Sonu <bismoy.sonu20@gmail.com>
2021-01-19 22:09:50 +0530

    Assign-D2

---
+++ W01D02-5Assignment.py
@@ -0,0 +1,56 @@
+#1
+open=14371.65
+high=14545.55
+low=14351.05
+close=14526.6
+print("Open=",open)
+print("High=",high)
+print("Low=",low)
+print("Close=",close)
+
+#2
+from datetime import date
+today=date.today()
+print("Today's date:",today)
+
+#3
+username="Bismoy"
+print(username)
+
+#4
+age=28
+print("Age=",age)
+
+#5
+print(username,age,sep=" ")
+
+#6
+'''
+1.Storing the O H L C prices for the day and printing it
+2.Printing today's date
+3.Printing username
+4.Printing age
+5.Printing username and age together separated by space.
+'''
+
+#7
+
+'''
+== will not work for assigning values to a variable as it is not an assignment operator 
+and instead it gives an error variable not defined.
+'''
+
+#8
+
+volume=0
+turnover=8
+print("Sum=",volume+turnover)
+
+#9
+
+1bis=98
+print(1bis)
+
+'''
+Above code gives invalid syntax error
+'''
---
+++ W01D02-debug.py
@@ -0,0 +1,37 @@
+#1
+turnover = 345
+print(turnover)
+
+#2
+volume_whole = 335098798
+print(volume_whole)
+
+#3
+
+n = 300
+print(n)
+
+#4
+_wonder = 1000
+print(_wonder/1000)
+
+#5
+a = b = c = 100
+d, e, f = 200,300,400
+g, h, i = 300, 400, 500
+print(a, b, c, d, e, f, g, h, i)
+
+#6
+a = b = 30
+print(a, b)
+
+#7
+
+a_g_e = 15
+print(a_g_e)
+
+#8
+
+numberofcollegegrads = 45
+Numberofcollegegrads = 45
+print(numberofcollegegrads, numberofcollegegrads)
